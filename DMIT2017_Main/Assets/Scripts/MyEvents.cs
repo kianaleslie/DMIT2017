4edc52b95193ed62bb472432cad16c36d805ab9c
kiana <kianaleslie25@gmail.com>
2024-02-07 14:28:50 -0700

    added events exercise // added events package example to DMIT2017_Main

---
+++ DMIT2017_Main/Assets/Scripts/MyEvents.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+static public class MyEvents
+{
+    static public UnityEvent Activate = new UnityEvent();
+    static public UnityEvent ChangeColor = new UnityEvent();
+}
---
+++ DMIT2017_Main/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float hInput, vInput, movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hInput * movementSpeed * Time.deltaTime, 0, vInput * movementSpeed * Time.deltaTime));
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            MyEvents.Activate.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            MyEvents.ChangeColor.Invoke();
+        }
+    }
+}
