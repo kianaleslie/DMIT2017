d191951c11fcbb40aca678c5ed578a35eaf899ae
kiana <kianaleslie25@gmail.com>
2024-03-25 15:52:29 -0600

    added scene manager lab

---
+++ KianaLeslieSceneManager/Assets/Scripts/LoadArea.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadArea : MonoBehaviour
+{
+    [SerializeField] string areaName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(areaName, LoadSceneMode.Additive);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.UnloadSceneAsync(areaName);
+        }
+    }
+}
---
+++ KianaLeslieSceneManager/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    float moveSpeed;
+    float rotateSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = 10.0f;
+        rotateSpeed = 50.0f;
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime);
+        rb.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * moveSpeed);
+    }
+}
